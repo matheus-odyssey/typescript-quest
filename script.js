f6678d9e6ccd99e9e8f86ed248dc28afb410957f
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-01 22:54:53 -0300

    Exercício 2

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 "use strict";
-function normalizarTexto(txt) {
-    return txt.trim().toLowerCase();
+const input = document.querySelector('input');
+const total = localStorage.getItem('total');
+if (input && total) {
+    const totalMudou = () => {
+        const value = input.value;
+        localStorage.setItem('total', value);
+    };
+    const calcularGanho = (value) => {
+        const p = document.querySelector('p');
+        if (p) {
+            p.innerText = `Ganho Total: R$ ${value + 100 - value * 0.2}`;
+        }
+    };
+    input.value = total;
+    calcularGanho(Number(input.value));
+    input.addEventListener('keyup', totalMudou);
 }
-const textoBruto = normalizarTexto('        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, COMO também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. se popularizou na dÉcaDA de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais RECENTEMENTE quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.       ');
-console.log(textoBruto);
