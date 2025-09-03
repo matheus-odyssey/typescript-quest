d7c5b6afd3ecd01b7afa138fc4cc150d00320eae
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-02 21:54:35 -0300

    Types e Interface 1

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 "use strict";
-// 1 - Crie uma função chamada toNumber
-// 2 - A função pode receber number | string
-// 3 - Se a função receber um número, retorne um número
-// 4 - Se a função receber uma string, retorne um número
-// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
-function toNumber(value) {
-    if (typeof value === 'number') {
-        return value;
-    }
-    if (typeof value === 'string') {
-        return Number(value);
-    }
-    throw new Error('value deve ser uma string ou number');
+let total = 20;
+total = '10';
+function preencherDados(dados) {
+    document.body.innerHTML += `
+  <div>
+    <h2>${dados.nome}</h2>
+    <p>R$ ${dados.preco}</p>
+    <p>Inclui Webcam: ${dados.webcam ? 'sim' : 'não'}</p>
+  </div>
+  `;
 }
-console.log(toNumber('150'));
-console.log(toNumber(340));
-// console.log(toNumber(true));
+const notebook = { nome: 'Notebook', preco: 3000, webcam: true };
+preencherDados(notebook);
+preencherDados({ nome: 'Computador', preco: 3000, webcam: false });
+function imprimirStack(stack) {
+    console.log(stack);
+}
+imprimirStack('back-end');
+imprimirStack('front-end');
+imprimirStack('fullstack');
--- script.ts
+++ script.ts
@@ -1,21 +1,38 @@
-// 1 - Crie uma função chamada toNumber
-// 2 - A função pode receber number | string
-// 3 - Se a função receber um número, retorne um número
-// 4 - Se a função receber uma string, retorne um número
-// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+// A palavra chave type cria um alias para um tipo customizado
+type NumberOrString = string | number;
 
-function toNumber(value: string | number) {
-  if (typeof value === 'number') {
-    return value;
-  }
+let total: NumberOrString = 20;
 
-  if (typeof value === 'string') {
-    return Number(value);
-  }
+total = '10';
 
-  throw new Error('value deve ser uma string ou number');
+type Produto = {
+  nome: string;
+  preco: number;
+  webcam: boolean;
+};
+
+function preencherDados(dados: Produto) {
+  document.body.innerHTML += `
+  <div>
+    <h2>${dados.nome}</h2>
+    <p>R$ ${dados.preco}</p>
+    <p>Inclui Webcam: ${dados.webcam ? 'sim' : 'não'}</p>
+  </div>
+  `;
 }
 
-console.log(toNumber('150'));
-console.log(toNumber(340));
-// console.log(toNumber(true));
+const notebook: Produto = { nome: 'Notebook', preco: 3000, webcam: true };
+
+preencherDados(notebook);
+
+preencherDados({ nome: 'Computador', preco: 3000, webcam: false });
+
+type Stacks = 'back-end' | 'front-end' | 'fullstack';
+
+function imprimirStack(stack: Stacks) {
+  console.log(stack);
+}
+
+imprimirStack('back-end');
+imprimirStack('front-end');
+imprimirStack('fullstack');
