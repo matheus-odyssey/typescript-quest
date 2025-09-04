6e603d0cb518d71fac5268192c6e5a85ca6b2f27
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-04 13:36:33 -0300

    Array 2

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 "use strict";
-// arrays
-const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-const valores = [10, 20, 'Taxas', 40, 50, 'Produto', 70, 80, 'Descontos', 100];
-function maioresQue50(data) {
-    return data.filter((n) => n > 50);
+// Defina a interface da API: https://api.origamid.dev/json/cursos.json e mostre os dados na tela.
+function mostrarCursos(cursos) {
+    let color;
+    cursos.forEach((curso) => {
+        if (curso.nivel === 'iniciante') {
+            color = 'blue';
+        }
+        if (curso.nivel === 'avancado') {
+            color = 'red';
+        }
+        document.body.innerHTML += `
+      <h1 style="color: ${color}";>${curso.nome}</h1>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+      <p>Tags: ${curso.tags.join(', ')}</p>
+      <p>Aulas: ${curso.idAulas.join(' | ')}</p>
+    `;
+    });
 }
-// data: (string | number)[]
-function filtrarValores(data) {
-    return data.filter((item) => typeof item === 'number');
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const data = await response.json();
+    mostrarCursos(data);
 }
-console.log(maioresQue50(numeros));
-console.log(filtrarValores(valores));
-const dados = [
-    ['Guerra Ininita', 2019],
-    ['Ultimato', 2019],
-];
+fetchCursos();
--- script.ts
+++ script.ts
@@ -1,21 +1,44 @@
-// arrays
+// Defina a interface da API: https://api.origamid.dev/json/cursos.json e mostre os dados na tela.
 
-const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-const valores = [10, 20, 'Taxas', 40, 50, 'Produto', 70, 80, 'Descontos', 100];
+// Existem apenas dois níveis de cursos, Iniciante (iniciante) e Avançado (avancado). Se for para iniciante pinte o título de azul, para avançado pinte de vermelho.
 
-function maioresQue50(data: number[]) {
-  return data.filter((n) => n > 50);
+interface Curso {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: boolean;
+  tags: string[];
+  idAulas: number[];
+  nivel: 'iniciante' | 'avancado';
 }
 
-// data: (string | number)[]
-function filtrarValores(data: Array<number | string>) {
-  return data.filter((item) => typeof item === 'number');
+function mostrarCursos(cursos: Curso[]) {
+  let color: string;
+
+  cursos.forEach((curso) => {
+    if (curso.nivel === 'iniciante') {
+      color = 'blue';
+    }
+
+    if (curso.nivel === 'avancado') {
+      color = 'red';
+    }
+
+    document.body.innerHTML += `
+      <h1 style="color: ${color}";>${curso.nome}</h1>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
+      <p>Tags: ${curso.tags.join(', ')}</p>
+      <p>Aulas: ${curso.idAulas.join(' | ')}</p>
+    `;
+  });
 }
 
-console.log(maioresQue50(numeros));
-console.log(filtrarValores(valores));
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  mostrarCursos(data);
+}
 
-const dados = [
-  ['Guerra Ininita', 2019],
-  ['Ultimato', 2019],
-];
+fetchCursos();
