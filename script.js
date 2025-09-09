18ef34fc61d6949dd5dc2a9050ac0dd83c27a0d2
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-09 13:29:20 -0300

    Generics

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 "use strict";
-const btn = document.querySelector('button');
-// function handleClick(this: HTMLButtonElement, event: MouseEvent) {
-//   console.log(this, event)
+// function retorno<variavel>(a: variavel): variavel {
+//   return a
 // }
-function handleClick(event) {
-    const { currentTarget } = event;
-    if (currentTarget instanceof HTMLElement) {
-        console.log(currentTarget.textContent);
+// console.log(retorno(2.567).toFixed(2))
+// console.log(retorno('Hello World').toLowerCase())
+// console.log(retorno(true).valueOf())
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const frutas = ['Banana', 'Pêra', 'Morango', 'Maçã', 'Uva', 'Abacaxi', 'Mamão', 'Melão', 'Kiwi', 'Limão'];
+function firstFive(arr) {
+    return arr.slice(0, 5);
+}
+console.log(firstFive(numeros));
+console.log(firstFive(frutas));
+function notNull(arg) {
+    if (arg !== null) {
+        return arg;
+    }
+    else {
+        return null;
     }
 }
-btn?.addEventListener('click', handleClick);
+console.log(notNull(200)?.toFixed(2));
+console.log(notNull('Hello World')?.toLowerCase());
+function tipoDado(data) {
+    return {
+        data,
+        type: typeof data,
+    };
+}
+console.log(tipoDado(300).type);
+console.log(tipoDado('Hello World').type);
+console.log(tipoDado(true).type);
+console.log(tipoDado(1e19).data);
