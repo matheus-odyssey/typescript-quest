bb25f362dbe89355553b2e5b95267ae5dc258c9a
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-11 18:07:44 -0300

    Functions 3

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
 "use strict";
-function somar(a, b, c) {
-    return a + b + (c ? c : 0);
-}
-console.log(somar(0.2, 0.8));
-const subtrair = (a, b) => a + b;
-console.log(subtrair(0.2, 0.8));
-function pintarTela(cor) {
-    document.body.style.backgroundColor = cor;
-}
-console.log(pintarTela('tomato'));
-const btn = document.querySelector('button');
-btn?.click();
-function isString(value) {
-    if (typeof value === 'string') {
-        return true;
+// Crie uma função que arredonda um valor passado para cima.
+// A função pode receber string ou number.
+// A função deve retornar o mesmo tipo que ela receber.
+function arredondarNumber(n) {
+    if (typeof n === 'number') {
+        return Math.ceil(n);
+    }
+    else {
+        return Math.ceil(Number(n)).toString();
     }
 }
-console.log(isString('oi'));
-console.log(isString(12));
-function abortar(msg) {
-    throw new Error(msg);
-}
-function calcular(forma) {
-    forma.perimetro(4);
-}
-function normalizar(arg) {
-    if (typeof arg === 'string') {
-        return arg.trim().toLowerCase();
-    }
-    return arg.map((item) => item.trim().toLowerCase());
-}
-console.log(normalizar('    HEllO woRLd  '));
-console.log(normalizar(['   BAnaNA', '  AbAcAxI         ']));
-function $(selector) {
-    return document.querySelector(selector);
-}
-$('a')?.href;
-$('video')?.volume;
+console.log(arredondarNumber(2.3));
+console.log(arredondarNumber('1.3'));
--- script.ts
+++ script.ts
@@ -1,73 +1,16 @@
-function somar(a: number, b: number, c?: number): number {
-  return a + b + (c ? c : 0)
-}
+// Crie uma função que arredonda um valor passado para cima.
+// A função pode receber string ou number.
+// A função deve retornar o mesmo tipo que ela receber.
 
-console.log(somar(0.2, 0.8))
-
-const subtrair = (a: number, b: number) => a + b
-
-console.log(subtrair(0.2, 0.8))
-
-type Callback = (event: MouseEvent) => void
-
-
-function pintarTela(cor: string): void {
-  document.body.style.backgroundColor = cor
-}
-
-console.log(pintarTela('tomato'))
-
-const btn = document.querySelector('button')
-
-btn?.click()
-
-
-function isString(value: any) {
-  if (typeof value === 'string') {
-    return true
+function arredondarNumber(n: number): number;
+function arredondarNumber(n: string): string;
+function arredondarNumber(n: number | string): number | string {
+  if (typeof n === 'number') {
+    return Math.ceil(n)
+  } else {
+    return Math.ceil(Number(n)).toString()
   }
 }
 
-console.log(isString('oi'));
-console.log(isString(12));
-
-
-function abortar(msg: string): never {
-  throw new Error(msg)
-}
-
-// abortar('Um erro ocorreu!')
-// console.log('Esse código não executa')
-
-interface Quadrado {
-  lado: 4;
-  perimetro(lado: number): number;
-}
-
-function calcular(forma: Quadrado) {
-  forma.perimetro(4)
-}
-
-// Overloads
-
-function normalizar(arg: string[]): string[];
-function normalizar(arg: string): string;
-function normalizar(arg: string | string[]) {
-  if (typeof arg === 'string') {
-    return arg.trim().toLowerCase()
-  }
-
-  return arg.map((item) => item.trim().toLowerCase())
-}
-
-console.log(normalizar('    HEllO woRLd  '))
-console.log(normalizar(['   BAnaNA', '  AbAcAxI         ']))
-
-function $(selector: 'a'): HTMLAnchorElement | null;
-function $(selector: 'video'): HTMLVideoElement | null;
-function $(selector: string): Element | null {
-  return document.querySelector(selector)
-}
-
-$('a')?.href
-$('video')?.volume
+console.log(arredondarNumber(2.3))
+console.log(arredondarNumber('1.3'))
