efa980d9fe37c7707fae7654582406d4ca423bd3
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-12 12:36:32 -0300

    Type Unknown

--- script.ts
+++ script.ts
@@ -1,5 +1,55 @@
-// Guard, Safety e Narrowing
+{
+  // Guard, Safety e Narrowing
 
+  function typeGuard(value: unknown) {
+    if (typeof value === 'string') {
+      return value.toLowerCase()
+    }
+
+    if (typeof value === 'number') {
+      return value.toFixed(1)
+    }
+  }
+
+  console.log(typeGuard(39))
+  console.log(typeGuard('HeLLo WOrlD'))
+
+  const obj = {
+    nome: 'João',
+    msg: 'hello world',
+  }
+
+  if ('msg' in obj) {
+    console.log('Existe')
+  }
+
+  interface Produto {
+    nome: string
+    preco: number
+  }
+
+  function handleProduto(data: Produto) {
+    console.log(data)
+
+    if ('preco' in data) {
+      document.body.innerHTML += `
+    <p>Nome: ${data.nome}</p>
+    <p>Preço: ${data.preco + 100}</p>
+  `
+    }
+  }
+
+  async function fetchProduto() {
+    const res = await fetch('https://api.origamid.dev/json/notebook.json')
+    const json = await res.json()
+
+    handleProduto(json)
+  }
+
+  fetchProduto()
+}
+
+// unknown
 function typeGuard(value: unknown) {
   if (typeof value === 'string') {
     return value.toLowerCase()
@@ -8,41 +58,14 @@ function typeGuard(value: unknown) {
   if (typeof value === 'number') {
     return value.toFixed(1)
   }
-}
 
-console.log(typeGuard(39))
-console.log(typeGuard('HeLLo WOrlD'))
-
-const obj = {
-  nome: 'João',
-  msg: 'hello world',
-}
-
-if ('msg' in obj) {
-  console.log('Existe')
-}
-
-interface Produto {
-  nome: string
-  preco: number
-}
-
-function handleProduto(data: Produto) {
-  console.log(data)
-
-  if ('preco' in data) {
-    document.body.innerHTML += `
-    <p>Nome: ${data.nome}</p>
-    <p>Preço: ${data.preco + 100}</p>
-  `
+  if (value instanceof HTMLElement) {
+    return value.innerText
   }
 }
 
-async function fetchProduto() {
-  const res = await fetch('https://api.origamid.dev/json/notebook.json')
-  const json = await res.json()
+// any desativa a verificação de tipos enquanto unknown exige fazer um Type Guard antes de manipular os dados
 
-  handleProduto(json)
-}
-
-fetchProduto()
+console.log(typeGuard('Olá, Mundo'))
+console.log(typeGuard(34.678))
+console.log(typeGuard(document.body))
