e9414ebfe1e389491ee780f20d458c9c10d3bd35
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-16 10:18:44 -0300

    User Type Guard 1

--- script.js
+++ script.js
@@ -1,52 +1,25 @@
 "use strict";
-{
-    // Guard, Safety e Narrowing
-    function typeGuard(value) {
-        if (typeof value === 'string') {
-            return value.toLowerCase();
-        }
-        if (typeof value === 'number') {
-            return value.toFixed(1);
-        }
+function handleCursos(data) {
+    if (data instanceof Array) {
+        console.log('É uma instância de Array');
     }
-    console.log(typeGuard(39));
-    console.log(typeGuard('HeLLo WOrlD'));
-    const obj = {
-        nome: 'João',
-        msg: 'hello world',
-    };
-    if ('msg' in obj) {
-        console.log('Existe');
-    }
-    function handleProduto(data) {
-        console.log(data);
-        if ('preco' in data) {
-            document.body.innerHTML += `
-    <p>Nome: ${data.nome}</p>
-    <p>Preço: ${data.preco + 100}</p>
-  `;
-        }
-    }
-    async function fetchProduto() {
-        const res = await fetch('https://api.origamid.dev/json/notebook.json');
-        const json = await res.json();
-        handleProduto(json);
-    }
-    fetchProduto();
-}
-// unknown
-function typeGuard(value) {
-    if (typeof value === 'string') {
-        return value.toLowerCase();
-    }
-    if (typeof value === 'number') {
-        return value.toFixed(1);
-    }
-    if (value instanceof HTMLElement) {
-        return value.innerText;
+    if (Array.isArray(data)) {
+        console.log('É uma array');
     }
 }
-// any desativa a verificação de tipos enquanto unknown exige fazer um Type Guard antes de manipular os dados
-console.log(typeGuard('Olá, Mundo'));
-console.log(typeGuard(34.678));
-console.log(typeGuard(document.body));
+async function fetchCursos() {
+    const res = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await res.json();
+    handleCursos(json);
+}
+fetchCursos();
+function isString(value) {
+    return typeof value === 'string';
+}
+function handleData(data) {
+    if (isString(data)) {
+        console.log(data.toLowerCase());
+    }
+}
+handleData(200);
+handleData('GitHub');
--- script.ts
+++ script.ts
@@ -1,71 +1,31 @@
-{
-  // Guard, Safety e Narrowing
-
-  function typeGuard(value: unknown) {
-    if (typeof value === 'string') {
-      return value.toLowerCase()
-    }
-
-    if (typeof value === 'number') {
-      return value.toFixed(1)
-    }
+function handleCursos(data: unknown) {
+  if (data instanceof Array) {
+    console.log('É uma instância de Array')
   }
 
-  console.log(typeGuard(39))
-  console.log(typeGuard('HeLLo WOrlD'))
-
-  const obj = {
-    nome: 'João',
-    msg: 'hello world',
-  }
-
-  if ('msg' in obj) {
-    console.log('Existe')
-  }
-
-  interface Produto {
-    nome: string
-    preco: number
-  }
-
-  function handleProduto(data: Produto) {
-    console.log(data)
-
-    if ('preco' in data) {
-      document.body.innerHTML += `
-    <p>Nome: ${data.nome}</p>
-    <p>Preço: ${data.preco + 100}</p>
-  `
-    }
-  }
-
-  async function fetchProduto() {
-    const res = await fetch('https://api.origamid.dev/json/notebook.json')
-    const json = await res.json()
-
-    handleProduto(json)
-  }
-
-  fetchProduto()
-}
-
-// unknown
-function typeGuard(value: unknown) {
-  if (typeof value === 'string') {
-    return value.toLowerCase()
-  }
-
-  if (typeof value === 'number') {
-    return value.toFixed(1)
-  }
-
-  if (value instanceof HTMLElement) {
-    return value.innerText
+  if (Array.isArray(data)) {
+    console.log('É uma array')
   }
 }
 
-// any desativa a verificação de tipos enquanto unknown exige fazer um Type Guard antes de manipular os dados
+async function fetchCursos() {
+  const res = await fetch('https://api.origamid.dev/json/cursos.json')
+  const json = await res.json()
 
-console.log(typeGuard('Olá, Mundo'))
-console.log(typeGuard(34.678))
-console.log(typeGuard(document.body))
+  handleCursos(json)
+}
+
+fetchCursos()
+
+function isString(value: unknown): value is string {
+  return typeof value === 'string'
+}
+
+function handleData(data: unknown) {
+  if (isString(data)) {
+    console.log(data.toLowerCase())
+  }
+}
+
+handleData(200)
+handleData('GitHub')
