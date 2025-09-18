b02dd80b6a96a34e355da27a408f4cb8aceb9d6a
Matheus Henrique <matheushnt.dev@gmail.com>
2025-09-18 11:47:18 -0300

    keyof 1

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
 "use strict";
-// Tuples são arrays que possuem dados em posições fixas
-const produto1 = ['Notebook', 2500];
-const produto2 = ['Televisão', 6500];
-console.log(produto2[0].toLowerCase());
-console.log(produto2[1].toFixed(2));
-// Torna um dado readonly e infere o tipo de dado mais específico possível. Em métodos que
-// retornam Array's, as mesmas são transformadas em Tuples.
-// const getText = (element: string): [HTMLElement, string] | null => {
-const getText = (element) => {
-    const el = document.querySelector(element);
-    if (el) {
-        return [el, el.innerText];
-    }
-    else {
-        return null;
-    }
+let chave;
+// let chave: 'nome' | 'preco' | 'lote'
+chave = "lote";
+// Já vimos o typeof do JavaScript, responsável por retornar o tipo do dado. No TypeScript
+// podemos utilizar ele para indicar que um dado possui o mesmo tipo que outro.
+function coordenadas(x, y) {
+    return { x, y, };
+}
+let coord;
+coord = (x, y) => {
+    return { x, y, };
 };
-console.log(getText(('a')));
-const handleVendas = (vendas) => {
-    if (vendas) {
-        let totalVendas = 0;
-        vendas.forEach((venda) => {
-            document.body.innerHTML += `
-        <h2>${venda[0].toUpperCase()}</h2>
-        <p>Vendas: R$ ${venda[1].toFixed(2)}</p>
-      `;
-            totalVendas += venda[1];
-        });
-        document.body.innerHTML += `Total de vendas: R$ ${totalVendas}`;
-    }
-};
-const fetchVendas = async (url) => {
-    const response = await fetch(url);
-    const json = await response.json();
-    handleVendas(json);
-};
-fetchVendas('https://api.origamid.dev/json/vendas.json');
+function selectElement(selector) {
+    return document.querySelector(selector);
+}
+const element = selectElement('body');
--- script.ts
+++ script.ts
@@ -1,60 +1,38 @@
-// Tuples são arrays que possuem dados em posições fixas
-
-const produto1 = ['Notebook', 2500]
-const produto2: [string, number] = ['Televisão', 6500]
-
-console.log(produto2[0].toLowerCase())
-console.log(produto2[1].toFixed(2))
-
-// Torna um dado readonly e infere o tipo de dado mais específico possível. Em métodos que
-// retornam Array's, as mesmas são transformadas em Tuples.
-
-// const getText = (element: string): [HTMLElement, string] | null => {
-const getText = (element: string) => {
-  const el = document.querySelector<HTMLElement>(element)
-
-  if (el) {
-    return [el, el.innerText] as const
-  } else {
-    return null
-  }
-}
-
-console.log(getText(('a')))
-
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-
+// Indica que o dado é uma chave de uma Interface/Tipo.
 interface Produto {
-  marca: string
-  cor: string
+  nome: string
+  preco: number
+  lote: string
 }
 
-type Venda = [string, number, string, Produto]
+let chave: keyof Produto
+// let chave: 'nome' | 'preco' | 'lote'
 
-const handleVendas = (vendas: Venda[]) => {
-  if (vendas) {
-    let totalVendas = 0
+chave = "lote"
 
-    vendas.forEach((venda) => {
-      document.body.innerHTML += `
-        <h2>${venda[0].toUpperCase()}</h2>
-        <p>Vendas: R$ ${venda[1].toFixed(2)}</p>
-      `
-
-      totalVendas += venda[1]
-    })
-
-    document.body.innerHTML += `Total de vendas: R$ ${totalVendas}`
-  }
+// Já vimos o typeof do JavaScript, responsável por retornar o tipo do dado. No TypeScript
+// podemos utilizar ele para indicar que um dado possui o mesmo tipo que outro.
+function coordenadas(x: number, y: number) {
+  return { x, y, }
 }
 
-const fetchVendas = async (url: string) => {
-  const response = await fetch(url)
-  const json = await response.json()
+let coord: typeof coordenadas
 
-  handleVendas(json)
+coord = (x: number, y: number) => {
+  return { x, y, }
 }
 
-fetchVendas('https://api.origamid.dev/json/vendas.json')
+
+interface Elements {
+  a: HTMLAnchorElement
+  div: HTMLDivElement
+  video: HTMLVideoElement
+  audio: HTMLAudioElement
+  body: HTMLBodyElement
+}
+
+function selectElement<K extends keyof Elements>(selector: K): Elements[K] | null {
+  return document.querySelector(selector)
+}
+
+const element = selectElement('body')
